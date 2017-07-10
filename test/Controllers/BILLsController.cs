b2061fe4ff3dcb46f51b42a013b0580f3d387b4e
unknown <383234436@qq.com>
2017-07-10 20:31:14 +0800

    first

---
+++ test/Controllers/BILLsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using test;
+
+namespace test.Controllers
+{
+    public class BILLsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: BILLs
+        public ActionResult Index()
+        {
+            return View(db.BILL.ToList());
+        }
+
+        // GET: BILLs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BILL bILL = db.BILL.Find(id);
+            if (bILL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bILL);
+        }
+
+        // GET: BILLs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BILLs/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ITEM_TYPE,COLUMN1")] BILL bILL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BILL.Add(bILL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bILL);
+        }
+
+        // GET: BILLs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BILL bILL = db.BILL.Find(id);
+            if (bILL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bILL);
+        }
+
+        // POST: BILLs/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ITEM_TYPE,COLUMN1")] BILL bILL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bILL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bILL);
+        }
+
+        // GET: BILLs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BILL bILL = db.BILL.Find(id);
+            if (bILL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bILL);
+        }
+
+        // POST: BILLs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            BILL bILL = db.BILL.Find(id);
+            db.BILL.Remove(bILL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ test/Controllers/helloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class helloWorldController : Controller
+    {
+        // GET: helloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("hello" + name + ",NumTimes is :" + numTimes);
+        }
+    }
+}
